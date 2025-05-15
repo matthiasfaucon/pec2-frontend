b5b2e8efe3d1adff71395cafc62d1beed21bbb3c
anthonyvouin <avouin@normandiewebschool.fr>
2025-05-15 16:43:57 +0200

    Update container.dart

--- lib/components/header/container.dart
+++ lib/components/header/container.dart
@@ -2,6 +2,8 @@ import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:firstflutterapp/config/router.dart';
+import 'package:provider/provider.dart';
+import 'package:firstflutterapp/notifiers/userNotififers.dart';
 
 class Header extends StatefulWidget {
   @override
@@ -10,16 +12,19 @@ class Header extends StatefulWidget {
 
 class _HeaderState extends State<Header> {
   Widget _buildHeader() {
+    final userNotifier = Provider.of<UserNotifier>(context);
+    final userName = userNotifier.user?.userName ?? "Utilisateur";
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Text("Salut,", style: TextStyle(fontSize: 16)),
+          children: [
+            const Text("Salut,", style: TextStyle(fontSize: 16)),
             Text(
-              "John Doe",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              userName,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
           ],
         ),
